52603a333a71fc157cc37f2f8707586be90b6a94
Wendy <wendy8wendy@hotmail.com>
2024-07-05 09:08:18 +0800

    1

---
+++ rosjc/rss.php
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+<channel>
+<title><![CDATA[EMLOG]]></title> 
+<description><![CDATA[使用emlog搭建的站点]]></description>
+<link>http://mikrotik.wendy-ip.com/</link>
+<language>zh-cn</language>
+<generator>www.emlog.net</generator>
+<item>
+    <title>MikroTik-RouterOS软路由使用教程</title>
+    <link>http://mikrotik.wendy-ip.com/?post=2</link>
+    <description><![CDATA[<p>[该文章已设置加密]</p>]]></description>
+    <pubDate>Fri, 05 Jul 2024 07:24:05 +0800</pubDate>
+    <author>emer</author>
+    <guid>http://mikrotik.wendy-ip.com/?post=2</guid>
+</item>
+<item>
+    <title>欢迎使用emlog</title>
+    <link>http://mikrotik.wendy-ip.com/?post=1</link>
+    <description><![CDATA[<p>[该文章已设置加密]</p>]]></description>
+    <pubDate>Fri, 05 Jul 2024 07:22:22 +0800</pubDate>
+    <author>emer</author>
+    <guid>http://mikrotik.wendy-ip.com/?post=1</guid>
+</item></channel>
+</rss>
