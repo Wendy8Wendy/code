557f30c083183df9ab2a31ab2dc3bbdd627f2398
文迪 <wendy8wendy@hotmail.com>
2024-09-18 08:39:20 +0800

    Create wd01.js

---
+++ js/wd01.js
@@ -0,0 +1,35 @@
+{
+        "@context": "https://schema.org",
+        "@type": "BlogPosting",
+        "datePublished": "2022-12-30T10:29:28+08:00",
+        "dateModified": "2023-11-08T16:31:52+08:00",
+        "url": "https://ikuriko.github.io/posts/cetnos7_pppoe/",
+        "headline": "CetnOS7 PPPoE Server",
+        "description": "在CentOS7上搭建PPPoE Server",
+        "inLanguage" : "zh-CN",
+        "articleSection": "posts",
+        "wordCount":  123 ,
+        "image": "https://ikuriko.github.io/icons/apple-touch-icon.png",
+        "author": {
+            "@type": "Person",
+            "description": "Mosi Meta",
+            "email": "ikuriko@qq.com",
+            "image": "https://ikuriko.github.io/icons/apple-touch-icon.png",
+            "url": "https://ikuriko.github.io",
+            "name": "ikuriko"
+        },
+        "license": "[CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh)",
+        "publisher": {
+            "@type": "Organization",
+            "name": "ikuriko",
+            "logo": {
+                "@type": "ImageObject",
+                "url": "https://ikuriko.github.io/icons/apple-touch-icon.png"
+            },
+            "url": "https://ikuriko.github.io/"
+        },
+        "mainEntityOfPage": {
+            "@type": "WebSite",
+            "@id": "https://ikuriko.github.io/"
+        }
+    }
